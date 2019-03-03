4ca1a0c8f9a14ac1dd3fe32c20deb818380948a8
Chih-Jung Chen <chenc@aldenv107.allegheny.edu>
2019-03-02 20:39:26 -0500

    added favicon

--- gator_grouper/urls.py
+++ gator_grouper/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, re_path, path
 
+
+
 urlpatterns = [
     re_path(r"^admin/", admin.site.urls),
     re_path(r"", include("gatorgrouper.urls")),
     re_path(r"^gatorgrouper/", include("gatorgrouper.urls")),
     re_path(r"^auth/", include('social_django.urls', namespace='social')),
-
+    
 ]
